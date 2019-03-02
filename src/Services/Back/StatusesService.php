e41eff17d19b494cda6ef3caed4fefb6f29968c7
Konstantin Bukin <bukin@inetstudio.ru>
2019-03-03 02:22:20 +0300

    Classifiers refactoring

--- src/Services/Back/StatusesService.php
+++ src/Services/Back/StatusesService.php
@@ -80,7 +80,7 @@ class StatusesService implements StatusesServiceContract
         $action = ($id) ? 'отредактирован' : 'создан';
         $item = $this->repository->save($request, $id);
 
-        app()->make('InetStudio\Classifiers\Contracts\Services\Back\ClassifiersServiceContract')
+        app()->make('InetStudio\Classifiers\Entries\Contracts\Services\Back\EntriesServiceContract')
             ->attachToObject($request, $item);
 
         $item->searchable();
