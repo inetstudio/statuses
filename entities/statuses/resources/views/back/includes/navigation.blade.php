41c735c38cc6d3e3a44eb5275d78f04e2a0a1796
Konstantin Bukin <bukin@inetstudio.ru>
2021-12-08 11:21:25 +0300

    Update

--- entities/statuses/resources/views/back/includes/navigation.blade.php
+++ entities/statuses/resources/views/back/includes/navigation.blade.php
@@ -1,4 +1,4 @@
-<li class="{{ isActiveRoute('back.statuses.*') }}">
+<li class="{{ isActiveRoute('back.statuses.*', 'mm-active') }}">
     <a href="{{ route('back.statuses.index') }}">
         <i class="fa fa-exchange-alt"></i> <span class="nav-label">Статусы</span>
     </a>
